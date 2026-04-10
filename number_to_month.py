fde1a75f04f46e0cd764332c84eae70931726da6
NahuelCiampichini <nnciampichini@mail.austral.edu.ar>
2026-04-10 10:36:16 -0300

    Add files via upload

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,31 @@
 # Replace the "ANSWER HERE" for your answer
 
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    numb = month
+    match numb:
+        case 1:
+            return "enero"
+        case 2:
+            return "febrero"
+        case 3:
+            return "marzo"
+        case 4:
+            return "abril"
+        case 5:
+            return "mayo"
+        case 6:
+            return "junio"
+        case 7:
+            return "julio"
+        case 8:
+            return "agosto"
+        case 9:
+            return "septiembre"
+        case 10:
+            return "octubre"
+        case 11:
+            return "noviembre"
+        case 12:
+            return "diciembre"
+        case _:
+            return "error"
--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,38 @@
-# Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
-
+    discrimi = (b ** 2) - 4 * a * c
+    if discrimi > 0:
+        r1 = (-b + ((b ** 2) - 4 * a * c) ** 0.5) / 2 * a
+        r2 = (-b - ((b ** 2) - 4 * a * c) ** 0.5) / 2 * a
+        return f"({r1}, {r2})"
+    elif discrimi == 0:
+        r12 = (-b + ((b ** 2) - 4 * a * c) ** 0.5) / 2 * a
+        return f"({r12})"
+    else:
+        return "( )"
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a*x**2 + b*x + c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    if a == 0 and b == 0:
+        return f"f(x) = {c}"
+    elif a == 0:
+        return f"f(x) = {b} * X + {c}"
+    elif b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    else:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    if a == 0:
+        return f"f'(x) = {b}"
+    elif a== 0 and b == 0:
+        return f"f'(x) = 0"
+    elif b == 0:
+        return f"f'(x) = {2*a} * X"
+    else:
+        return f"f'(x) = {2*a} * X + {b}"
